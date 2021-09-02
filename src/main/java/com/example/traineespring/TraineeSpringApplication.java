1d75cfdb6088156f58d96db6c9549701afb5a725
chekitostein <bamba954@gmail.com>
2021-09-02 08:34:51 +0300

    start

---
+++ src/main/java/com/example/traineespring/TraineeSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.traineespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TraineeSpringApplication.class, args);
+    }
+
+}
